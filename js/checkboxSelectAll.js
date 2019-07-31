9e247a8f9a8428ddadbf8747121c3e77324ca907
victornachdenken <18850042763@163.com>
2019-07-31 18:46:34 +0800

    select all checkbox or unselect all checkbox with one click

---
+++ js/checkboxSelectAll.js
@@ -0,0 +1,12 @@
+﻿$(function(){
+	//一键全选、取消全选
+	//<input type="checkbox" id="checkboxAll"/>全选
+	$("#checkboxAll").change(function(){
+		var all = document.getElementById("checkboxAll");
+		var ch = document.getElementsByName("xxxxxxCheckbox");
+		for(var i=0;i<ch.length;i++) {
+			ch[i].checked = all.checked;
+		}
+	});
+})
+	

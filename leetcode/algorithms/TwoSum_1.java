3dbf6b0bfe1cdbdcfcc8fecf6758ec7c764d6060
victornachdenken <18850042763@163.com>
2019-07-31 18:03:38 +0800

    fixed encode problem

--- leetcode/algorithms/TwoSum_1.java
+++ leetcode/algorithms/TwoSum_1.java
@@ -1,4 +1,4 @@
-package leetcode.algorithms;
+ï»¿package leetcode.algorithms;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,16 +13,16 @@ public class TwoSum_1 {
 		}
 	}
 	static int[] twoSum(int[] numbers, int target) {
-		//1¡¢Èôtarget=9,int[] numbers = {2, 7, 11, 15, 34, 5}
+		//1ã€è‹¥target=9,int[] numbers = {2, 7, 11, 15, 34, 5}
 	    int[] result = new int[2];
 	    Map<Integer, Integer> map = new HashMap<Integer, Integer>();
 	    for (int i = 0; i < numbers.length; i++) {
-	        if (map.containsKey(target - numbers[i])) {//3¡¢9-7=2
+	        if (map.containsKey(target - numbers[i])) {//3ã€9-7=2
 	            result[1] = i;
 	            result[0] = map.get(target - numbers[i]);
 	            return result;
 	        }
-	        map.put(numbers[i], i);///2¡¢i=0,(2,0)
+	        map.put(numbers[i], i);///2ã€i=0,(2,0)
 	    }
 	    return result;
 	}
